1cd7ad352a262538fa09a06f133f370785a030b2
guntur <fxguntur04@gmail.com>
2025-06-21 13:33:14 +0700

    add: Test Decode JSON

---
+++ decode_json_test.go
@@ -0,0 +1,23 @@
+package belajargolangjson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecodeJSON(t *testing.T) {
+	jsonString := `{"FirstName":"Bobi","LastName":"Bubi","Age":10,"Married":false}`
+	jsonBytes := []byte(jsonString)
+
+	customer := &Customer{}
+
+	err := json.Unmarshal(jsonBytes, customer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
+	fmt.Println(customer.FirstName)
+	fmt.Println(customer.Age)
+	fmt.Println(customer.Married)
+}
